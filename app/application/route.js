40d156667b04e6c37364636893290efe4bdf839c
Paul McConnell <pmc.consult@gmail.com>
2015-07-12 14:47:51 -0500

    Moved application route and template to pod. Added page titles.

---
+++ app/application/route.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  title: function(tokens) {
+     var base = 'Babblesort';
+     var hasTokens = tokens && tokens.length;
+
+     return hasTokens ? (base + ' - ' + tokens.reverse().join(' | '))  : base;
+    }
+});
