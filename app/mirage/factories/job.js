40026878387dfe317f9269a44df3dd285d873aef
Paul McConnell <pmc.consult@gmail.com>
2015-08-11 21:01:23 -0500

    Typo fix

--- app/mirage/factories/job.js
+++ app/mirage/factories/job.js
@@ -3,5 +3,5 @@ import Mirage/*, {faker} */ from 'ember-cli-mirage';
 export default Mirage.Factory.extend({
   company: 'company name',
   description: 'job description',
-  skills: ['c#', 'asp.net mvc', 'ember', 'angular' 'javascript', 'sql server']
+  skills: ['c#', 'asp.net mvc', 'ember', 'angular', 'javascript', 'sql server']
 });
