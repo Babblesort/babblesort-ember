37d19dc411b4cee6f1fa93084ec4111ce17412ce
Paul McConnell <pmc.consult@gmail.com>
2015-08-15 18:58:40 -0500

    bulked up the about this site list. Prep for animating details about a lib entry.

--- app/about/route.js
+++ app/about/route.js
@@ -4,10 +4,38 @@ export default Ember.Route.extend({
   titleToken: 'About This Site',
   model: function() {
     let buildItems = Ember.A([
-      'ember js',
-      'ember-cli',
-      'ember-cli-materialize',
-      'liquid-fire'
+      {
+        lib: 'ember js',
+        details: {
+          image: '',
+          description: 'javascript framework for creating ambitious applications.',
+          url: 'http://ember.js'
+        }
+      },
+      {
+        lib: 'ember-cli',
+        details: {
+          image: '',
+          description: 'command line interface for creating and building ember applications.',
+          url: 'http://ember.js'
+        }
+      },
+      {
+        lib: 'ember-cli-materialize',
+        details: {
+          image: '',
+          description: 'CSS and component library based on Material Design.',
+          url: ''
+        }
+      },
+      {
+        lib: 'liquid-fire',
+        details: {
+          image: '',
+          description: 'Animation and transition library for ember applications.',
+          url: 'http://ember.js'
+        }
+      }
     ]);
     return buildItems;
   }
