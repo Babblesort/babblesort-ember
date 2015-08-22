54950117615cb1075d6f5a4defeb80b7027ad1b5
Paul McConnell <pmc.consult@gmail.com>
2015-08-21 21:10:27 -0500

    use new about item model in about route and display component

--- app/about/route.js
+++ app/about/route.js
@@ -1,43 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
   titleToken: 'About This Site',
+
   model: function() {
-    let buildItems = Ember.A([
-      {
-        lib: 'ember js',
-        details: {
-          image: '',
-          description: 'javascript framework for creating ambitious applications.',
-          url: 'http://ember.js'
-        }
-      },
-      {
-        lib: 'ember-cli',
-        details: {
-          image: '',
-          description: 'command line interface for creating and building ember applications.',
-          url: 'http://ember.js'
-        }
-      },
-      {
-        lib: 'ember-cli-materialize',
-        details: {
-          image: '',
-          description: 'CSS and component library based on Material Design.',
-          url: ''
-        }
-      },
-      {
-        lib: 'liquid-fire',
-        details: {
-          image: '',
-          description: 'Animation and transition library for ember applications.',
-          url: 'http://ember.js'
-        }
-      }
-    ]);
-    return buildItems;
+    return this.store.findAll('about-site-item');
   }
 
 });
