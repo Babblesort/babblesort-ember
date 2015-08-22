d2bf6c65752618d3bd7321902947317a0d0732ca
Paul McConnell <pmc.consult@gmail.com>
2015-08-21 21:10:55 -0500

    add rest adapter and mirage fixture file

---
+++ app/mirage/fixtures/aboutSiteItems.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 1,
+    libraryName: 'ember.js',
+    version: '1.13.7',
+    image: 'img url',
+    description: 'javascript framework for creating ambitious applications.',
+    url: 'library url'
+  },
+  {
+    id: 2,
+    libraryName: 'ember-cli',
+    version: '1.13.8',
+    image: 'img url',
+    description: 'command line interface for creating and building ember applications.',
+    url: 'library url'
+  },
+  {
+    id: 3,
+    libraryName: 'ember-cli-materialize',
+    version: '0.17.3',
+    image: 'img url',
+    description: 'CSS and component library based on Material Design.',
+    url: 'library url'
+  },
+  {
+    id: 4,
+    libraryName: 'liquid-fire',
+    version: '0.21.2',
+    image: 'img url',
+    description: 'Animation and transition library for ember applications',
+    url: 'library url'
+  }
+];
