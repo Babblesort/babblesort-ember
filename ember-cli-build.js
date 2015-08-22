580d769006d355f9bf9f11b4af2472c6718e7017
Paul McConnell <pmc.consult@gmail.com>
2015-08-22 17:41:25 -0500

    fixed deprecation warning by moving sassOptions to ember-cli-build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,18 +1,11 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
-// var pickFiles = require('broccoli-static-compiler');
-// var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, { });
-
-  // app.import('bower_components/bootstrap-sass-official/assets/javascripts/bootstrap.js');
-  //
-  // var bootstrapFonts = pickFiles('bower_components/bootstrap-sass-official/assets/fonts/bootstrap', {
-  //     srcDir: '/',
-  //     destDir: 'fonts/bootstrap'
-  // });
-
+  var app = new EmberApp(defaults, {
+    sassOptions: {
+      includePaths: ['bower_components/materialize/sass']
+    }
+   });
   return app.toTree();
-  //return mergeTrees([app.toTree(), bootstrapFonts]);
 };
